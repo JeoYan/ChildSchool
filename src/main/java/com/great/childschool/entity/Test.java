f86b65c971937325c05511b62abe690f040fefbd
JeoYan <215476838@qq.com>
2019-12-16 20:09:29 +0800

    springboot初始框架搭建，搭建一半，写了几个没用的test文件，请不用使用

---
+++ src/main/java/com/great/childschool/entity/Test.java
@@ -0,0 +1,18 @@
+package com.great.childschool.entity;
+
+/**
+ * 架构测试，请不用填写
+ *
+ */
+
+public class Test
+{
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/great/childschool/service/Test.java
@@ -0,0 +1,18 @@
+package com.great.childschool.service;
+
+/**
+ * 架构测试，请不用填写
+ *
+ */
+
+public class Test
+{
+
+
+
+
+
+
+
+
+}
