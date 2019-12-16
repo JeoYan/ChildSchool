6fe595cd7d14fede5e3e72ba220e278a3278e617
JeoYan <215476838@qq.com>
2019-12-16 17:50:25 +0800

    Initial commit

---
+++ src/main/java/com/great/childschool/ChildschoolApplication.java
@@ -0,0 +1,15 @@
+package com.great.childschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildschoolApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ChildschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/great/childschool/ChildschoolApplicationTests.java
@@ -0,0 +1,15 @@
+package com.great.childschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildschoolApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
