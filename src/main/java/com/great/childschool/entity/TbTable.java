7d6af6fc093662e57ec92b818852c5f01215f9ce
zhi0127 <2297984043@qq.com>
2019-12-17 12:03:55 +0800

    汤建志1217-1

---
+++ src/main/java/com/great/childschool/entity/TbTable.java
@@ -0,0 +1,60 @@
+package com.great.childschool.entity;
+
+public class TbTable
+{
+	private String code;
+	private String msg;
+	private String count;
+	private Object data;
+
+
+	public TbTable()
+	{
+	}
+
+	public String getCode()
+	{
+		return code;
+	}
+
+	public void setCode(String code)
+	{
+		this.code = code;
+	}
+
+	public String getMsg()
+	{
+		return msg;
+	}
+
+	public void setMsg(String msg)
+	{
+		this.msg = msg;
+	}
+
+	public String getCount()
+	{
+		return count;
+	}
+
+	public void setCount(String count)
+	{
+		this.count = count;
+	}
+
+	public Object getData()
+	{
+		return data;
+	}
+
+	public void setData(Object data)
+	{
+		this.data = data;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "TbTable{" + "code='" + code + '\'' + ", msg='" + msg + '\'' + ", count='" + count + '\'' + ", data=" + data + '}';
+	}
+}
---
+++ src/main/java/com/great/childschool/mapper/BackMapper.java
@@ -0,0 +1,62 @@
+package com.great.childschool.mapper;
+
+
+
+
+
+import com.great.childschool.entity.TbLog;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Mapper
+public interface BackMapper
+{
+
+//	public TbAdmin findByTbAdmin(TbAdmin admin);
+//
+//	public List<TbUser> findByTbUser(Map<String, Object> map);
+//
+//	public int findByTbUserNum(Map<String, Object> map);
+//
+//	public int deleteTbUserById(int userId);
+//
+//	public int addTbUser(TbUser user);
+//
+//	public int updateUser(TbUser user);
+//
+//	public int updateAdmin(TbAdmin admin);//事务管理测试
+//
+//	public List<TbMenu> findMenu(int personID);
+//
+	public List<TbLog> findLog(Map<String, Object> map);
+
+	public int findLogNum(Map<String, Object> map);
+
+	public int addLog(TbLog log);
+
+//	public int upload(TbDocument document);
+//
+//	public List<TbMenu> findFirstMenu(int personID);
+//
+//	public List<TbMenu> findChildMenu(Map<String, Object> map);
+//
+//	//更新树
+//	public int updateTree(Map<String, Object> map);
+//
+//	/**树置为未选*/
+//	public int clearChecked(int updateRoleId);
+//
+//
+//	public List<TbRole> findRole();
+//
+//	public int findLoginRole(int adminId);
+//
+//	public List<TbDocument> findByTbDocument(Map<String, Object> map);
+//
+//	public int findByTbDocumentNum(Map<String, Object> map);
+
+
+}
