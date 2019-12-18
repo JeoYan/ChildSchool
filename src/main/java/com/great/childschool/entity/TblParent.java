20ac499c7c0b299a9109f9e64c1eb1c51ef13585
JeoYan <215476838@qq.com>
2019-12-18 09:42:20 +0800

    完成家长的登入，及菜单显示----严俊杰

---
+++ src/main/java/com/great/childschool/entity/TblParent.java
@@ -0,0 +1,72 @@
+package com.great.childschool.entity;
+
+
+public class TblParent {
+
+  private int pid;
+  private String pname;
+  private String ppsw;
+  private String pphone;
+  private String pjob;
+  private int sid;
+
+  public int getPid()
+  {
+    return pid;
+  }
+
+  public void setPid(int pid)
+  {
+    this.pid = pid;
+  }
+
+  public String getPname()
+  {
+    return pname;
+  }
+
+  public void setPname(String pname)
+  {
+    this.pname = pname;
+  }
+
+  public String getPpsw()
+  {
+    return ppsw;
+  }
+
+  public void setPpsw(String ppsw)
+  {
+    this.ppsw = ppsw;
+  }
+
+  public String getPphone()
+  {
+    return pphone;
+  }
+
+  public void setPphone(String pphone)
+  {
+    this.pphone = pphone;
+  }
+
+  public String getPjob()
+  {
+    return pjob;
+  }
+
+  public void setPjob(String pjob)
+  {
+    this.pjob = pjob;
+  }
+
+  public int getSid()
+  {
+    return sid;
+  }
+
+  public void setSid(int sid)
+  {
+    this.sid = sid;
+  }
+}
