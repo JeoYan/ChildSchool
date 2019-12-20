2c4a60cecb2478e1fb818f8f1e1318c6a32f8bcb
ChaoLove123 <578707495@qq.com>
2019-12-20 11:37:58 +0800

    幼儿管理的增删改查 家长管理的删和查

---
+++ src/main/java/com/great/childschool/entity/CcTblParentBaby.java
@@ -0,0 +1,48 @@
+package com.great.childschool.entity;
+
+
+public class CcTblParentBaby
+{
+
+  private long pbid;
+  private long pid;
+  private long bid;
+  private String prelation;
+
+
+  public long getPbid() {
+    return pbid;
+  }
+
+  public void setPbid(long pbid) {
+    this.pbid = pbid;
+  }
+
+
+  public long getPid() {
+    return pid;
+  }
+
+  public void setPid(long pid) {
+    this.pid = pid;
+  }
+
+
+  public long getBid() {
+    return bid;
+  }
+
+  public void setBid(long bid) {
+    this.bid = bid;
+  }
+
+
+  public String getPrelation() {
+    return prelation;
+  }
+
+  public void setPrelation(String prelation) {
+    this.prelation = prelation;
+  }
+
+}
