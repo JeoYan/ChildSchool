cf26c6c504ea73035fa9e635fa17d5f98db8fb8d
chenmeijie <250668856@qq.com>
2020-01-09 15:25:57 +0800

    陈美杰车牌识别

--- src/main/java/com/great/childschool/controller/CmjClassInfoController.java
+++ src/main/java/com/great/childschool/controller/CmjClassInfoController.java
@@ -2,15 +2,22 @@ package com.great.childschool.controller;
 
 import com.great.childschool.entity.CmjTblBaby;
 import com.great.childschool.entity.DataBean;
+import com.great.childschool.entity.TjzTbTable;
 import com.great.childschool.service.CmjClassInfoService;
+import com.great.childschool.tools.Base64Util;
+import com.great.childschool.tools.Constants;
 import com.great.childschool.tools.FaceSpot;
+import com.great.childschool.tools.HttpUtil;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
@@ -62,6 +69,32 @@ public class CmjClassInfoController
 	public List<Object> queryBabyInfo(int bid){
 		List<Object> babyFamily = classInfoService.findBabyFamily(bid);
 		return babyFamily;
+	}
+
+	/**
+	 * 上传车牌号图片
+	 *
+	 * @param file
+	 * @return
+	 */
+	@RequestMapping("/licensePlate")
+	@ResponseBody
+	public Object uploadFile(MultipartFile file) {
+		try {
+			byte[] b = file.getBytes();
+			String base64 = Base64Util.encode(b);
+			String param = URLEncoder.encode("image", Constants.UTF8) + "=" + URLEncoder.encode(base64, Constants.UTF8);
+			String result = HttpUtil.post(Constants.BAIDU_AI_LICENSE_PLATE, Constants.BAIDU_AI_TOKEN, param);
+			String number = com.alibaba.fastjson.JSONObject.parseObject(result).getJSONObject("words_result").getString("number");
+			System.out.println("识别出的车牌号为：" + number);
+			com.alibaba.fastjson.JSONObject object = new com.alibaba.fastjson.JSONObject();
+			object.put("code", 0);
+			object.put("number", number);
+			return object;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	@RequestMapping("/searchFace")
